e7737c34031732d50ebc4223306742fe1ce19ad1
root <root@1417935-cz96471.tw1.ru>
2023-05-18 12:48:47 +0300

    remove print from settings

--- web_form/web_form/settings.py
+++ web_form/web_form/settings.py
@@ -28,7 +28,6 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.getenv('DEBUG') == 'True' else False
-print(DEBUG)
 
 ALLOWED_HOSTS = ['saving-tracker.andrey-agas-portfolio.website',]
 CSRF_TRUSTED_ORIGINS = ['https://saving-tracker.andrey-agas-portfolio.website',]
